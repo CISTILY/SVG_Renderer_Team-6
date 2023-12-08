714a20dcfd5242f7a5549b8e0b2a32395d008063
CISTILY <lebathanh.nv@gmail.com>
2023-12-08 08:26:27 +0700

    Fixing points replacement in path rendering

--- SVGRenderer/SVGRenderer.cpp
+++ SVGRenderer/SVGRenderer.cpp
@@ -24,7 +24,7 @@ VOID OnPaint(HDC hdc)
     // Read XML
     xml_document<> doc;
     xml_node<>* rootNode;
-    string filename = "svg-03.svg";
+    string filename = "svg-18.svg";
     // Read the xml file into a vector
     ifstream file(filename);
     vector<char> buffer((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
@@ -78,34 +78,6 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
     WNDCLASS            wndClass;
     GdiplusStartupInput gdiplusStartupInput;
     ULONG_PTR           gdiplusToken;
-
-    
-
-
-    //// Read XML
-    //xml_document<> doc;
-    //xml_node<>* rootNode;
-    //// Read the xml file into a vector
-    //ifstream file("sample.svg");
-    //vector<char> buffer((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-    //buffer.push_back('\0');
-    //// Parse the buffer using the xml file parsing library into doc 
-    //doc.parse<0>(&buffer[0]);
-
-    //rootNode = doc.first_node();
-    //xml_node<>* node = rootNode->first_node();
-
-    //while (node != NULL) {
-    //    char* nodeName = node->name();
-    //    xml_attribute<>* firstAttribute = node->first_attribute();
-    //    char* attributeName = firstAttribute->name();
-    //    char* attributeValue = firstAttribute->value();
-    //    xml_attribute<>* secondAttribute = firstAttribute->next_attribute();
-    //    // Set breakpoint here to view value
-    //    // Ref: http://rapidxml.sourceforge.net/manual.html
-    //    node = node->next_sibling();
-    //}
-
 
 
     // Initialize GDI+.
