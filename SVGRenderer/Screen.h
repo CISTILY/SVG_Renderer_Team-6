1cb494c5d72ffc4a8b282e66c8f6945f85ef0b73
Phuc0210 <128725672+Phuc0210@users.noreply.github.com>
2023-12-11 02:14:49 +0700

    Create Screen.h

---
+++ SVGRenderer/Screen.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector> 
+#include "rapidxml.hpp"
+
+using namespace rapidxml;
+using namespace std;
+
+class ScreenSVG
+{
+private:
+    float width;
+    float height;
+    float view_box[4];
+
+    bool flagWidth;
+    bool flagHeight;
+    bool flagViewBox;
+
+public:
+    ScreenSVG();
+  
+    ~ScreenSVG();
+    
+    void buildScreen(vector<char*>, vector<char*>);
+    void readScreen(xml_node<>* node);
+};
