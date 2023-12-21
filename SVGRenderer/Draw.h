1cb7a3ea62bc33ffff5bfc983875f1f58aefab5f
Phuc0210 <128725672+Phuc0210@users.noreply.github.com>
2023-12-22 02:52:37 +0700

    change to singleton and composite pattern

--- SVGRenderer/Draw.h
+++ SVGRenderer/Draw.h
@@ -10,20 +10,23 @@ class Draw {
 private:
     static bool drew;
     static vector< GraphicsPath*> graphicsPaths;
+    int curent_path;
 public:
     Draw();
     ~Draw();
     
     void transform(Graphics&, Shape*);
+
+    void drawShape(Graphics&, vector<Shape*> shapesSVG);
+
     VOID DrawCircle(Graphics&, CircleSVG);
     VOID DrawRectangle(Graphics&, RectangleSVG);
     VOID DrawEllipse(Graphics&, EllipseSVG);
     VOID DrawLine(Graphics&, LineSVG);
     VOID DrawText(Graphics&, TextSVG);
     VOID DrawPolygon(Graphics&, PolygonSVG);
-    VOID DrawPath(Graphics&, PathSVG, int&); 
+    VOID DrawPath(Graphics&, PathSVG); 
     VOID DrawPolyline(Graphics&, PolylineSVG);
-    void drawShape(Graphics&, vector<ShapeData>);
 
     static void setDrew(bool);
 };
